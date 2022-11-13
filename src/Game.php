2fe8b56ae758b5a58569c38d91aa6d6e821bf7c7
mitsuruyoshizumi <mitsuruyoshizumi@yahoo.co.jp>
2022-11-13 15:26:01 +0900

    Bowling Game Kata

---
+++ src/Game.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App;
+
+class Game
+{
+    const FRAMES_PER_GAME = 10;
+
+    protected array $rolls = [];
+
+    public function roll(int $pins)
+    {
+        $this->rolls[] = $pins;
+    }
+
+    public function score()
+    {
+        $score = 0;
+        $roll = 0;
+
+        foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
+
+            if ($this->isStrike($roll)) {
+                $score += $this->pinCount($roll) + $this->strikeBonus($roll);
+
+                $roll += 1;
+
+                continue;
+            }
+
+            $score += $this->defaultFrameScore($roll);
+
+            if ($this->isSpare($roll)) {
+                $score += $this->spareBonus($roll);
+            }
+
+            $roll += 2;
+        }
+
+        return $score;
+    }
+
+    protected function isStrike(int $roll): bool
+    {
+        return $this->pinCount($roll) === 10;
+    }
+
+    protected function isSpare(int $roll): bool
+    {
+        return $this->defaultFrameScore($roll) === 10;
+    }
+
+    protected function defaultFrameScore(int $roll): int
+    {
+        return $this->pinCount($roll) + $this->pinCount($roll + 1);
+    }
+
+    protected function strikeBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 1) + $this->pinCount($roll + 2);
+
+        // return $this->defaultFrameScore($roll + 1);
+    }
+
+    protected function spareBonus(int $roll): int
+    {
+        return $this->pinCount($roll + 2);
+    }
+
+    protected function pinCount(int $roll): int
+    {
+        return $this->rolls[$roll];
+    }
+}
